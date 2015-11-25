c127ee5cefdbe37c0cc276c5b5f766d8beb07be6
Isaac Grey <isaacgreywebdeveloper@gmail.com>
2015-11-25 15:44:48 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 
-Stamplay.init('sdkqueryobject');
+Stamplay.init('yourAppId');
 
 function queryObject(){
  	var cars = document.getElementById("cars");
  	var selectedCar = cars.options[cars.selectedIndex].value;
 
-	var objectCollection = new Stamplay.Cobject('object').Collection;
-	objectCollection.equalTo("brand", selectedCar).fetch().then(function(res) {
+	var objectCollection = new Stamplay.Cobject('objectModel').Collection;
+	objectCollection.equalTo("property", selectedCar).fetch().then(function(res) {
 		alert(objectCollection.instance[0].instance.brand);
 	});
 }
